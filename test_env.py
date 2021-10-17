e8867635d5eb4f229e066e31819781e9f017b5c8
clukedurham <clukedurham@gmail.com>
2021-10-17 17:24:57 -0400

    More rooms added

--- test_env.py
+++ test_env.py
@@ -10,23 +10,70 @@ def action(command):
 		elif(i.startswith('error')):
 			return False
 
-# Set up a small house with a door.
+# Create Bob's House
 action('CreatePlace(BobsHouse, Cottage)')
 action('CreateCharacter(Bob, B)')
+action('CreateItem(Sword, Sword)')
+action('SetPosition(Sword, BobsHouse.Table)')
+action('EnableIcon("Pick_Up", draw, Sword, "Pick up Sword", true)')
 action('SetClothing(Bob, Peasant)')
 action('SetPosition(Bob, BobsHouse.Door)')
-action('EnableIcon("Open_Door", Open, BobsHouse.Door, "Leave the house", true)')
+action('EnableIcon("Open_Door", exit, BobsHouse.Door, "Go to City", true)')
 action('ShowMenu()')
+
+# Create City
+action('CreatePlace(City, City)')
+action('EnableIcon("Open_Door", exit, City.GreenHouseDoor, "Go to Bob\'s house", true)')
+# Create Blacksmith
+action('EnableIcon("Open_Door", exit, City.BrownHouseDoor, "Go to Black Smith", true)')
+action('CreatePlace(Blacksmith, Blacksmith)')
+action('EnableIcon("Open_Door", exit, Blacksmith.Door, "Go to City", true)')
+# Create Tavern
+action('EnableIcon("Open_Door", exit, City.RedHouseDoor, "Go to Tavern", true)')
+action('CreatePlace(Tavern, Tavern)')
+action('EnableIcon("Open_Door", exit, Tavern.Door, "Go to City", true)')
+# Create Alchemy Shop
+action('EnableIcon("Open_Door", exit, City.BlueHouseDoor, "Go to Alchemy Shop", true)')
+action('CreatePlace(AlchemyShop, AlchemyShop)')
+action('EnableIcon("Open_Door", exit, AlchemyShop.Door, "Go to City", true)')
 
 # Respond to input.
 while(True):
 	i = input()
-	if(i == 'input Selected Start'):
+	if(i == 'input Selected Start' or i == 'input Selected Resume'):
 		action('SetCameraFocus(Bob)')
 		action('HideMenu()')
 		action('EnableInput()')
-	elif(i == 'input Open_Door BobsHouse.Door'):
-		action('SetNarration("The door is locked!")')
-		action('ShowNarration()')
+	elif(i == 'input Pick_Up Sword'):
+		action('Draw(Bob, Sword)')
+		# TODO add sword to inventory
+	elif(i == 'input Open_Door BobsHouse.Door'): # enter bob's house
+		action('Exit(Bob, BobsHouse.Door, true)')
+		action('Enter(Bob, City.GreenHouseDoor, true)')
+	elif(i == 'input Open_Door City.GreenHouseDoor'): # exit bob's house
+		action('Exit(Bob, City.GreenHouseDoor, true)')
+		action('Enter(Bob, BobsHouse.Door, true)')
+	elif(i == 'input Open_Door City.BrownHouseDoor'): # enter Blacksmith
+		action('Exit(Bob, City.BrownHouseDoor, true)')
+		action('Enter(Bob, Blacksmith.Door, true)')
+	elif(i == 'input Open_Door Blacksmith.Door'): # exit Blacksmith
+		action('Exit(Bob, Blacksmith.Door, true)')
+		action('Enter(Bob, City.BrownHouseDoor, true)')
+	elif(i == 'input Open_Door City.RedHouseDoor'): # enter Tavern
+		action('Exit(Bob, City.RedHouseDoor, true)')
+		action('Enter(Bob, Tavern.Door, true)')
+	elif(i == 'input Open_Door Tavern.Door'): # exit Tavern
+		action('Exit(Bob, Tavern.Door, true)')
+		action('Enter(Bob, City.RedHouseDoor, true)')
+	elif(i == 'input Open_Door City.BlueHouseDoor'): # enter AlchemyShop
+		action('Exit(Bob, City.BlueHouseDoor, true)')
+		action('Enter(Bob, AlchemyShop.Door, true)')
+	elif(i == 'input Open_Door AlchemyShop.Door'): # exit AlchemyShop
+		action('Exit(Bob, AlchemyShop.Door, true)')
+		action('Enter(Bob, City.BlueHouseDoor, true)')
+	elif(i == 'input Key Pause'):
+		action('ShowMenu()')
+	elif(i == 'input Selected Quit'):
+		action('Quit()')
 	elif(i == 'input Close Narration'):
 		action('HideNarration()')
